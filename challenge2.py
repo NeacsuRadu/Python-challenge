81922e3c8203352d6d78ac284de27552890e37a9
NeacsuRadu <rneacsu@bitdefender.com>
2019-04-17 23:32:05 +0300

    first two challenges solved

---
+++ challenge2.py
@@ -0,0 +1,12 @@
+text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+v1 = "abcdefghijklmnopqrstuvwxyz"
+v2 = "cdefghijklmnopqrstuvwxyzab"
+result = text.translate(str.maketrans(v1, v2))
+print(result)
+
+url = "map"
+result = url.translate(str.maketrans(v1, v2))
+print(result)
+
+# http://www.pythonchallenge.com/pc/def/ocr.html
